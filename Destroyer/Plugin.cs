e9343ab92b1bf9569f20e2aa38fb722556a99d8f
White <monsieur_white@hotmail.com>
2015-04-26 11:51:19 +0930

    Prime integration

--- Destroyer/Plugin.cs
+++ Destroyer/Plugin.cs
@@ -35,10 +35,21 @@ namespace Destroyer
 
 	    private void OnGameUpdate(EventArgs args)
 	    {
+			//destroyer
 		    foreach (var npc in Main.npc.Where(n => n.active && n.type == 134))
-		    {
+			{
+				if (npc.position.X / 16f > Main.maxTilesX || npc.position.X / 16f < 0)
+				{
+					continue;
+				}
+
 			    for (var i = -1; i < 4; i++)
 			    {
+				    if ((npc.position.Y/16f) + i > Main.maxTilesY || (npc.position.Y) + i < 0)
+				    {
+					    continue;
+				    }
+
 				    if (Main.tile[(int) (npc.position.X/16f), (int) (npc.position.Y/16f) + i].active())
 				    {
 					    WorldGen.KillTile((int) (npc.position.X/16f), (int) (npc.position.Y/16f) + i);
@@ -46,6 +57,48 @@ namespace Destroyer
 				    }
 			    }
 		    }
+
+			//prime
+		    foreach (var npc in Main.npc.Where(n => n.active && n.type < 132 && n.type > 126))
+			{
+				if (npc.position.X / 16f > Main.maxTilesX || npc.position.X / 16f < 0)
+				{
+					continue;
+				}
+
+				if (npc.type == 127)
+				{
+					for (var i = -4; i < 4; i++)
+					{
+						if ((npc.position.Y / 16f) + i > Main.maxTilesY || (npc.position.Y) + i < 0)
+						{
+							continue;
+						}
+
+						if (Main.tile[(int)(npc.position.X / 16f), (int)(npc.position.Y / 16f) + i].active())
+						{
+							WorldGen.KillTile((int)(npc.position.X / 16f), (int)(npc.position.Y / 16f) + i);
+							TSPlayer.All.SendTileSquare((int)(npc.position.X / 16f), (int)(npc.position.Y / 16f) + i);
+						}
+					}
+				}
+				else
+				{
+					for (var i = -1; i < 4; i++)
+					{
+						if ((npc.position.Y/16f) + i > Main.maxTilesY || (npc.position.Y) + i < 0)
+						{
+							continue;
+						}
+
+						if (Main.tile[(int) (npc.position.X/16f), (int) (npc.position.Y/16f) + i].active())
+						{
+							WorldGen.KillTile((int) (npc.position.X/16f), (int) (npc.position.Y/16f) + i);
+							TSPlayer.All.SendTileSquare((int) (npc.position.X/16f), (int) (npc.position.Y/16f) + i);
+						}
+					}
+				}
+			}
 	    }
     }
 }
